639a82042cab3811418f69e8059d46bf0402d461
Adam Csapo <csapo.adam@gmail.com>
2019-04-09 17:27:52 +0200

    more experimentation w/ specializations

--- 06-ora/Templates/main.cpp
+++ 06-ora/Templates/main.cpp
@@ -5,31 +5,31 @@
 #include "matrix.h"
 #include <string>
 
-Matrix<int> createMatrixWOnes(int nrows, int ncols) {
+Matrix<int,1> createMatrixWOnes(int nrows, int ncols) {
 	std::cout << "creating matrix w/ ones" << std::endl;
-	Matrix<int> m = Matrix<int>(nrows, ncols, 1);
+	Matrix<int,1> m = Matrix<int,1>(nrows, ncols, 1);
 	std::cout << "returning new matrix" << std::endl;
 	return m;
 }
 
-Matrix<int> createMatrixWTwos(int nrows, int ncols) {
+Matrix<int,1> createMatrixWTwos(int nrows, int ncols) {
 	std::cout << "creating matrix w/ twos" << std::endl;
-	Matrix<int> m = Matrix<int>(nrows, ncols, 2);
+	Matrix<int,1> m = Matrix<int,1>(nrows, ncols, 2);
 	std::cout << "returning new matrix" << std::endl;
 	return m;
 }
 
 void worker() {
-	Matrix<int> m1(3, 4, 0);
+	Matrix<int,1> m1(3, 4, 0);
 	m1[2][3] = 10;
 	std::cout << "m1 created" << std::endl;
 
 	//Matrix m2 = m1;
-	Matrix<int> m2(m1); // ugyanazt jelenti
+	Matrix<int,1> m2(m1); // ugyanazt jelenti
 	m2[2][3] = 9; // ez mindket matrixot irja!
 	std::cout << "m2 created" << std::endl;
 
-	Matrix<int> m3(3, 4, 0);
+	Matrix<int,1> m3(3, 4, 0);
 	m3 = m2; // m3 itt egy mar letezo objektum
 			 // ezert: nem konstrukcio, hanem ertekadas
 			 // muszaj: assignment operator!
@@ -41,14 +41,14 @@ void worker() {
 
 	// move konstruktor kikenyszeritheto explicit std::move-val (ha letezik move konstr)
 	// ha nem definialtuk a move konstr-t, ugyanugy copy constr fog meghivodni
-	Matrix<int> m4(std::move(m3)); // azt allitjuk, hogy m3-ra
+	Matrix<int,1> m4(std::move(m3)); // azt allitjuk, hogy m3-ra
 							  // mar nem lesz szukseg - hibas felhasznalas! (t.keppen)
 	m4[2][3] = 7;
 	std::cout << "m4 created" << std::endl;
 
 	// ha teheti, szinten move konstruktort hasznal
 	// ha nincs olyan, megteszi a copy is...
-	Matrix<int> m5(createMatrixWOnes(3, 4));
+	Matrix<int,1> m5(createMatrixWOnes(3, 4));
 	// destruktor is meghivodik, mivel eloszor kimasolja a fv-bol
 	// az eredmenyt es utana megszunteti
 	m5[2][3] = 6;
@@ -64,19 +64,19 @@ void worker() {
 	m4.print();
 	m5.print();
 
-	Matrix<int> m6(3,3,5);
+	Matrix<int,1> m6(3,3,5);
 	m6.print();
 	m6.timesTwo();
 	m6.print();
 
-	Matrix<char> m7(3, 3, 'a');
+	Matrix<char,2> m7(3, 3, 'a');
 	m7.print();
 	m7.timesTwo();
 	m7.print();
 	timesThree(m7);
 	m7.print();
 
-	Matrix<std::string> m8(3, 3, "a");
+	Matrix<std::string,1> m8(3, 3);
 	m8.print();
 	m8.timesTwo();
 	m8.print();
--- 06-ora/Templates/matrix.h
+++ 06-ora/Templates/matrix.h
@@ -1,12 +1,13 @@
 #pragma once
 #include <iostream>
 
-template <typename T>
+template <typename T, int v>
 class Matrix {
 private:
 	const int nrows;
 	const int ncols;
 	T** data;
+	int intvalue;
 	void unsafeCopyValuesFrom(const Matrix& other);
 public:
 	Matrix(int rows, int cols);
@@ -18,14 +19,15 @@ public:
 	Matrix& operator=(Matrix&& other);
 	void print();
 	T* operator[](int rowinx);
+
 	void timesTwo();
 
-	template <typename T>
-	friend void timesThree(Matrix<T>& m);
+	template <typename T, int v>
+	friend void timesThree(Matrix<T,v>& m);
 };
 
-template <typename T>
-void Matrix<T>::timesTwo() {
+template <typename T, int v>
+void Matrix<T, v>::timesTwo() {
 	for (int r = 0; r < nrows; r++) {
 		for (int c = 0; c < ncols; c++) {
 			data[r][c] = data[r][c] * 2; // duck typing - does T have a * binary operator?
@@ -33,8 +35,9 @@ void Matrix<T>::timesTwo() {
 	}
 }
 
+
 template <>
-void Matrix<std::string>::timesTwo() {
+void Matrix<std::string, 1>::timesTwo() {
 	for (int r = 0; r < nrows; r++) {
 		for (int c = 0; c < ncols; c++) {
 			data[r][c] = data[r][c].append(data[r][c]);
@@ -42,8 +45,25 @@ void Matrix<std::string>::timesTwo() {
 	}
 }
 
-template <typename T>
-void timesThree(Matrix<T>& m) {
+// Ezt sajnos nem lehet...
+// C++ templates are not limited to classes - they can also be used to define function templates.
+// Although function templates can be fully specialized, they cannot be partially specialized, 
+// irrespective of whether they are member function templates or non - member function templates.
+// This can be beneficial to compiler writers, but affects the flexibility and granularity of
+// what developers can do.
+/*
+template <int v>
+void Matrix<std::string, v>::timesTwo() {
+	for (int r = 0; r < nrows; r++) {
+		for (int c = 0; c < ncols; c++) {
+			data[r][c] = data[r][c].append(data[r][c]);
+		}
+	}
+}
+*/
+
+template <typename T, int v>
+void timesThree(Matrix<T, v>& m) {
 	for (int r = 0; r < m.nrows; r++) {
 		for (int c = 0; c < m.ncols; c++) {
 			m[r][c] = m[r][c] * 3; // duck typing - does T have a * binary operator?
@@ -52,7 +72,7 @@ void timesThree(Matrix<T>& m) {
 }
 
 template <>
-void timesThree(Matrix<std::string>& m) {
+void timesThree(Matrix<std::string, 1>& m) {
 	for (int r = 0; r < m.nrows; r++) {
 		for (int c = 0; c < m.ncols; c++) {
 			std::string cur(m[r][c]);
@@ -62,27 +82,21 @@ void timesThree(Matrix<std::string>& m) {
 }
 
 
-template <typename T>
-Matrix<T>::Matrix(int rows, int cols) :
-	nrows(rows), ncols(cols)
+template <typename T, int v>
+Matrix<T, v>::Matrix(int rows, int cols) :
+	Matrix(rows, cols, 0)
 {
-	try {
-		data = new T*[nrows];
-		for (int r = 0; r < nrows; r++) {
-			data[r] = new T[ncols];
-			for (int c = 0; c < ncols; c++) {
-				T val = 0; // duck typing - feltetelezzuk, hogy 0 ertek adhato T tipusnak!!
-				data[r][c] = val;
-			}
-		}
-	}
-	catch (std::bad_alloc& ba) {
-		std::cout << "Out of memory :(" << std::endl;
-	}
 }
 
-template <typename T>
-Matrix<T>::Matrix(int rows, int cols, T fillValue) : nrows(rows), ncols(cols) {
+template <>
+Matrix<std::string, 1>::Matrix(int rows, int cols) :
+	Matrix(rows, cols, std::string())
+{
+}
+
+
+template <typename T, int v>
+Matrix<T, v>::Matrix(int rows, int cols, T fillValue) : nrows(rows), ncols(cols), intvalue(v) {
 	try {
 		data = new T*[nrows];
 		for (int r = 0; r < nrows; r++) {
@@ -97,8 +111,8 @@ Matrix<T>::Matrix(int rows, int cols, T fillValue) : nrows(rows), ncols(cols) {
 	}
 }
 
-template <typename T>
-Matrix<T>::Matrix(const Matrix& other) :
+template <typename T, int v>
+Matrix<T, v>::Matrix(const Matrix& other) :
 	Matrix(other.nrows, other.ncols)
 	// constructor delegation
 	// csak inicializalo listaban lehet, kulonben
@@ -108,8 +122,8 @@ Matrix<T>::Matrix(const Matrix& other) :
 	this->unsafeCopyValuesFrom(other);
 }
 
-template <typename T>
-Matrix<T>::Matrix(Matrix&& other) :
+template <typename T, int v>
+Matrix<T, v>::Matrix(Matrix&& other) :
 	Matrix(other.nrows, other.ncols) {
 	std::cout << "Move constructor called" << std::endl;
 	T** olddata = data;
@@ -119,8 +133,8 @@ Matrix<T>::Matrix(Matrix&& other) :
 						  // ezert tok jok vagyunk!!
 }
 
-template <typename T>
-Matrix<T>& Matrix<T>::operator=(const Matrix& other) {
+template <typename T, int v>
+Matrix<T, v>& Matrix<T, v>::operator=(const Matrix& other) {
 	// inicializalni mar nem kell, hiszen mar
 	// inicializalva van ez az objektum
 	// viszont kell dimension check!!
@@ -134,8 +148,8 @@ Matrix<T>& Matrix<T>::operator=(const Matrix& other) {
 	return *this;
 }
 
-template <typename T>
-Matrix<T>& Matrix<T>::operator=(Matrix&& other) {
+template <typename T, int v>
+Matrix<T, v>& Matrix<T, v>::operator=(Matrix&& other) {
 	// inicializalni mar nem kell, hiszen mar
 	// inicializalva van ez az objektum
 	// viszont kell dimension check!!
@@ -149,8 +163,8 @@ Matrix<T>& Matrix<T>::operator=(Matrix&& other) {
 	return *this;
 }
 
-template <typename T>
-Matrix<T>::~Matrix() {
+template <typename T, int v>
+Matrix<T, v>::~Matrix() {
 	std::cout << "Destructor called" << std::endl;
 	for (int r = 0; r < nrows; r++) {
 		delete[] data[r];
@@ -158,8 +172,9 @@ Matrix<T>::~Matrix() {
 	delete[] data;
 }
 
-template <typename T>
-void Matrix<T>::print() {
+template <typename T, int v>
+void Matrix<T, v>::print() {
+	std::cout << "matrix w/ intvalue " << intvalue << std::endl;
 	for (int r = 0; r < nrows; r++) {
 		for (int c = 0; c < ncols - 1; c++) {
 			std::cout << data[r][c] << ", ";
@@ -169,16 +184,16 @@ void Matrix<T>::print() {
 	std::cout << std::endl;
 }
 
-template <typename T>
-T* Matrix<T>::operator[](int rowinx) {
+template <typename T, int v>
+T* Matrix<T, v>::operator[](int rowinx) {
 	if (rowinx < nrows && rowinx > -1) {
 		return data[rowinx];
 	}
 	return nullptr;
 }
 
-template <typename T>
-void Matrix<T>::unsafeCopyValuesFrom(const Matrix& other) {
+template <typename T, int v>
+void Matrix<T, v>::unsafeCopyValuesFrom(const Matrix& other) {
 	for (int r = 0; r < nrows; r++) {
 		for (int c = 0; c < ncols; c++) {
 			data[r][c] = other.data[r][c];
